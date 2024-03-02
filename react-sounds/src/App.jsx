e50f69af601f54c7065e0b9b9ead1d9fa7806c5b
Anadevega74 <151641554+Anadevega74@users.noreply.github.com>
2024-03-02 14:17:51 +0100

    Corregidos errores

--- react-sounds/src/App.jsx
+++ react-sounds/src/App.jsx
@@ -1,28 +1,30 @@
 import './App.css';
 
 function App() {
+
   const audios = {
-    E: new Audio(audioE),
-    A: new Audio(audioA),
-    D: new Audio(audioD),
-    G: new Audio(audioG),
-    B: new Audio(audioB),
-    F: new Audio(audioF),
+    E: new Audio(audios),
+    A: new Audio(audios),
+    D: new Audio(audios),
+    G: new Audio(audios),
+    B: new Audio(audios),
+    F: new Audio(audios),
   }
 
   function play(acorde){
     audios[acorde].play();
   }
+
   return (
     <>
     <h1>ACORDES GUITARRA</h1>   
       <div>
         <button className="E" onClick={() => play('E')}> E</button>
-        <button className="A" onClick={() => play('A')}> E</button>
-        <button className="D" onClick={() => play('D')}> E</button>
-        <button className="G" onClick={() => play('G')}> E</button>
-        <button className="B" onClick={() => play('B')}> E</button>
-        <button className="F" onClick={() => play('F')}> E</button>
+        <button className="A" onClick={() => play('A')}> A</button>
+        <button className="D" onClick={() => play('D')}> D</button>
+        <button className="G" onClick={() => play('G')}> G</button>
+        <button className="B" onClick={() => play('B')}> B</button>
+        <button className="F" onClick={() => play('F')}> F</button>
       </div>
      </>
   );
