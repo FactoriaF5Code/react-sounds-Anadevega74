4044a938b53eb116313086c2b71b7efb4ac2bc28
Anadevega74 <151641554+Anadevega74@users.noreply.github.com>
2024-03-02 12:29:49 +0100

    Insertando acordes

--- react-sounds/src/App.jsx
+++ react-sounds/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const audios = {
+    E: new Audio(audioE),
+    A: new Audio(audioA),
+    D: new Audio(audioD),
+    G: new Audio(audioG),
+    B: new Audio(audioB),
+    F: new Audio(audioF),
+  }
 
+  function play(acorde){
+    audios[acorde].play();
+  }
   return (
     <>
+    <h1>ACORDES GUITARRA</h1>   
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button className="E" onClick={() => play('E')}> E</button>
+        <button className="A" onClick={() => play('A')}> E</button>
+        <button className="D" onClick={() => play('D')}> E</button>
+        <button className="G" onClick={() => play('G')}> E</button>
+        <button className="B" onClick={() => play('B')}> E</button>
+        <button className="F" onClick={() => play('F')}> E</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+     </>
+  );
 }
 
 export default App;
